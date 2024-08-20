7d7622fd6c335efb8705ac011a95fe927987116d
Gayathri Hrudhi <gayathrihrudhi19@gmail.com>
2024-08-20 14:48:16 -0500

    Added chat and call tabs

--- apoio.py
+++ apoio.py
@@ -19,6 +19,7 @@ st.set_page_config(
 def load_data():
     df = pd.read_csv('dashboard_data.csv')
     df['date'] = pd.to_datetime(df['date'], errors='coerce')
+    df['duration'] = df['duration'].astype('float')
     return df
 
 def load_css():
@@ -69,101 +70,128 @@ filtered_df = df[
     (df['chatType'].isin(selected_chattype))
 ]
 
-st.write("### KPI Key Metrics")
 
-col1, col2, col3, col4 = st.columns(4)
+tab1, tab2, tab3 = st.tabs(["Dashboard", "Chats", "Calls"])
 
-with col1:
-    st.metric("Total Users", filtered_df['userId'].nunique())
+with tab1:
 
-with col2:
-    st.metric("Total Calls", filtered_df['callId'].count())
+    st.write("### KPI Key Metrics")
 
-with col3:
-    st.metric("Total Chats", filtered_df['chatId'].count())
+    col1, col2, col3, col4 = st.columns(4)
 
-with col4:
-    avg_duration = filtered_df['duration'].mean()
-    st.metric("Avg Call Duration (sec)", f"{avg_duration:.2f}" if pd.notna(avg_duration) else "N/A")
+    with col1:
+        st.metric("Total Users", filtered_df['userId'].nunique())
 
-with st.container():
-    st.write("### Province-wise categories")
-    bar_chart_fig = px.bar(
-                filtered_df.groupby(['province', 'category']).size().reset_index(name='count'),
-                x='province',
-                y='count',
-                color='category',
-                barmode="stack", 
-                category_orders={'province': ['Maputo City','Maputo','Gaza','Inhambane','Manica','Sofala','Tete','Zambezia','Nampula','Cabo Delgado','Niassa']},
-                text_auto=True,
+    with col2:
+        st.metric("Total Calls", filtered_df['callId'].count())
+
+    with col3:
+        st.metric("Total Chats", filtered_df['chatId'].count())
+
+    with col4:
+        avg_duration = filtered_df['duration'].mean()
+        st.metric("Avg Call Duration (sec)", f"{avg_duration:.2f}" if pd.notna(avg_duration) else "N/A")
+
+    with st.container():
+        st.write("### Province-wise categories")
+        bar_chart_fig = px.bar(
+                    filtered_df.groupby(['province', 'category']).size().reset_index(name='count'),
+                    x='province',
+                    y='count',
+                    color='category',
+                    barmode="stack", 
+                    category_orders={'province': ['Maputo City','Maputo','Gaza','Inhambane','Manica','Sofala','Tete','Zambezia','Nampula','Cabo Delgado','Niassa']},
+                    text_auto=True,
+                )
+        st.plotly_chart(bar_chart_fig, use_container_width=True)
+
+        st.write("##")
+        
+        st.write("### Age distribution across Provinces")
+        scatter_chart_fig = px.scatter(filtered_df, x='age', y='province', color='category', size='duration', log_x=True, size_max=60)
+        st.plotly_chart(scatter_chart_fig, use_container_width=True)
+
+
+        st.write("### Chat Type Distribution")
+        pie_chart_fig = px.pie(filtered_df, names='chatType')
+        st.plotly_chart(pie_chart_fig, use_container_width=True)
+
+        st.write("### Age vs. Height/Weight with Category Hue")
+        scatter_plot_fig = px.scatter(filtered_df, x='age', y='height', color='category', size='weight', hover_data=['weight'])
+        st.plotly_chart(scatter_plot_fig, use_container_width=True)
+
+
+        st.write("### Chats over Time")
+        group_line = (
+                filtered_df.groupby(["date"])
+                .count()['chatId']
+                .reset_index()
             )
-    st.plotly_chart(bar_chart_fig, use_container_width=True)
+        line_chart_fig = px.line(group_line, x='date', y='chatId')
+        st.plotly_chart(line_chart_fig, use_container_width=True)
 
-    st.write("##")
-    
-    st.write("### Age distribution across Provinces")
-    scatter_chart_fig = px.scatter(filtered_df, x='age', y='province', color='category', size='duration', log_x=True, size_max=60)
-    st.plotly_chart(scatter_chart_fig, use_container_width=True)
+        st.write("### Geographical Distribution of Calls/Chats")
 
+        grouped_df = filtered_df.groupby(['latitude', 'longitude', 'category', 'countryName', 'province', 'thoroughfare']).size().reset_index(name='num_cases')
 
-    st.write("### Chat Type Distribution")
-    pie_chart_fig = px.pie(filtered_df, names='chatType')
-    st.plotly_chart(pie_chart_fig, use_container_width=True)
+        map_fig2 = px.scatter_mapbox(grouped_df, 
+                                    lat='latitude', 
+                                    lon='longitude', 
+                                    color='category', 
+                                    size='num_cases',
+                                    hover_name='countryName', 
+                                    hover_data=['province', 'thoroughfare', 'num_cases'],
+                                    mapbox_style="open-street-map", 
+                                    zoom=3)
 
-    st.write("### Age vs. Height/Weight with Category Hue")
-    scatter_plot_fig = px.scatter(filtered_df, x='age', y='height', color='category', size='weight', hover_data=['weight'])
-    st.plotly_chart(scatter_plot_fig, use_container_width=True)
+        st.plotly_chart(map_fig2, use_container_width=True)
 
+        # map_fig = px.scatter_mapbox(filtered_df, lat='latitude', lon='longitude', color='category', size='duration',
+        #                             hover_name='countryName', hover_data=['province', 'thoroughfare'],
+        #                             mapbox_style="open-street-map", zoom=3)
+        # st.plotly_chart(map_fig, use_container_width=True)
 
-    st.write("### Chats over Time")
-    group_line = (
-            filtered_df.groupby(["date"])
-            .count()['chatId']
-            .reset_index()
-        )
-    line_chart_fig = px.line(group_line, x='date', y='chatId')
-    st.plotly_chart(line_chart_fig, use_container_width=True)
+        
+        # map_fig_chat = px.scatter_mapbox(filtered_df, lat='latitude', lon='longitude', color='chatType', size='age',
+        #                             hover_name='countryName', hover_data=['province', 'thoroughfare'],
+        #                             mapbox_style="open-street-map", zoom=3)
+        # st.plotly_chart(map_fig_chat, use_container_width=True)
 
-    st.write("### Geographical Distribution of Calls/Chats")
+        
+        st.write("### Location Density of Calls")
+        heatmap_fig = px.density_mapbox(filtered_df, lat='latitude', lon='longitude', z='duration',
+                                        radius=10, center=dict(lat=-20, lon=36),
+                                        mapbox_style="open-street-map", zoom=3)
+        st.plotly_chart(heatmap_fig)
 
-    grouped_df = filtered_df.groupby(['latitude', 'longitude', 'category', 'countryName', 'province', 'thoroughfare']).size().reset_index(name='num_cases')
+        
+        st.write("### Province-wise drilldown")
+        group_sun = (
+                filtered_df.groupby(["province", "category", "chatType", 'gender'])
+                .size()
+                .reset_index(name="count")
+            )
+        sunburst_fig = px.sunburst(group_sun, path=['province', 'category', 'chatType', 'gender'], values='count',  width=700, height=700)
+        st.plotly_chart(sunburst_fig, use_container_width=True)
 
-    map_fig2 = px.scatter_mapbox(grouped_df, 
-                                lat='latitude', 
-                                lon='longitude', 
-                                color='category', 
-                                size='num_cases',
-                                hover_name='countryName', 
-                                hover_data=['province', 'thoroughfare', 'num_cases'],
-                                mapbox_style="open-street-map", 
-                                zoom=3)
+with tab2:
+    chat_filtered_df = filtered_df[filtered_df.chatId.notna()]
+    for index, row in chat_filtered_df.iterrows():
+        with st.expander(f"Chat {row['chatId']}"):
+            st.write(f'{row['transcription'][:100]}...')
 
-    st.plotly_chart(map_fig2, use_container_width=True)
+            # Button to show full transcription
+            if st.button(f"Show full transcription of Chat {row['chatId']}"):
+                st.write(f'Category - {row['category']}')
+                st.write(row['transcription'])
 
-    # map_fig = px.scatter_mapbox(filtered_df, lat='latitude', lon='longitude', color='category', size='duration',
-    #                             hover_name='countryName', hover_data=['province', 'thoroughfare'],
-    #                             mapbox_style="open-street-map", zoom=3)
-    # st.plotly_chart(map_fig, use_container_width=True)
+with tab3:
+    call_filtered_df = filtered_df[filtered_df.callId.notna()]
+    for index, row in call_filtered_df.iterrows():
+        with st.expander(f"Call {row['callId']}"):
+            st.write(f'{row['transcription'][:100]}...')
 
-    
-    # map_fig_chat = px.scatter_mapbox(filtered_df, lat='latitude', lon='longitude', color='chatType', size='age',
-    #                             hover_name='countryName', hover_data=['province', 'thoroughfare'],
-    #                             mapbox_style="open-street-map", zoom=3)
-    # st.plotly_chart(map_fig_chat, use_container_width=True)
-
-    
-    st.write("### Location Density of Calls")
-    heatmap_fig = px.density_mapbox(filtered_df, lat='latitude', lon='longitude', z='duration',
-                                    radius=10, center=dict(lat=-20, lon=36),
-                                    mapbox_style="open-street-map", zoom=3)
-    st.plotly_chart(heatmap_fig)
-
-    
-    st.write("### Province-wise drilldown")
-    group_sun = (
-            filtered_df.groupby(["province", "category", "chatType", 'gender'])
-            .size()
-            .reset_index(name="count")
-        )
-    sunburst_fig = px.sunburst(group_sun, path=['province', 'category', 'chatType', 'gender'], values='count',  width=700, height=700)
-    st.plotly_chart(sunburst_fig, use_container_width=True)
+            # Button to show full transcription
+            if st.button(f"Show full transcription of Call {row['callId']}"):
+                st.write(f'Category - {row['category']}')
+                st.write(row['transcription'])
